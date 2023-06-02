3b386d011d740728f9a2567db346919cded58afe
kuzmichev-stepa <kuzmichev.stepa@gmail.com>
2023-06-02 23:18:34 +0200

    Добавлена параметризация и xfail

--- test_product_page.py
+++ test_product_page.py
@@ -3,11 +3,12 @@ from .pages.product_page import ProductPage
 import pytest
 import time
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('num', [*range(1,7), pytest.param(7, marks=pytest.mark.xfail(reason="Некорректен по условию задания")), *range(8,10)])
+def test_guest_can_add_product_to_basket(browser,num):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}"
     page = ProductPage(browser, link)
     page.open()
+    time.sleep(5)
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_messages_on_page()
