507eaf396024b11bcdd56c077886182df3a10217
kuzmichev-stepa <kuzmichev.stepa@gmail.com>
2023-06-06 01:29:00 +0200

    Добавлены тесты добавления вкорзину для авторизованного пользователя

--- test_product_page.py
+++ test_product_page.py
@@ -4,52 +4,84 @@ from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 import pytest
 import time
+import random
 
-#@pytest.mark.parametrize('num', [*range(1,7), pytest.param(7, marks=pytest.mark.xfail(reason="Некорректен по условию задания")), *range(8,10)])
-#def test_guest_can_add_product_to_basket(browser,num):
-#    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.add_product_to_basket()
-#    page.solve_quiz_and_get_code()
-#    page.should_be_messages_on_page()
-#
-#@pytest.mark.xfail(reason = "Некорректен по условию задания")    
-#def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.add_product_to_basket()
-#    page.should_not_be_success_message()
-#    
-#def test_guest_cant_see_success_message(browser):
-#    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.should_not_be_success_message()
-#
-#@pytest.mark.xfail(reason = "Некорректен по условию задания")    
-#def test_message_disappeared_after_adding_product_to_basket(browser):
-#    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.add_product_to_basket()
-#    page.should_be_disappeared_success_message()
-#    
-#def test_guest_should_see_login_link_on_product_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.should_be_login_link()
-#
-#def test_guest_can_go_to_login_page_from_product_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.go_to_login_page()
-#    login_page = LoginPage(browser, browser.current_url)
-#    login_page.should_be_login_page()
+@pytest.mark.user_add_to_basket
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function",autouse=True)
+    def setup(self, browser):
+        browser.implicitly_wait(10)
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time()) + str(random.random())
+        link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+    
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_messages_on_page()
+    
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
 
+@pytest.mark.need_review
+@pytest.mark.parametrize('num', [*range(1,7), pytest.param(7, marks=pytest.mark.xfail(reason="Некорректен по условию задания")), *range(8,10)])
+def test_guest_can_add_product_to_basket(browser,num):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_messages_on_page()
+
+@pytest.mark.xfail(reason = "Некорректен по условию задания")    
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+    
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail(reason = "Некорректен по условию задания")    
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_be_disappeared_success_message()
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
